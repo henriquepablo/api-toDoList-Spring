2a804f0bfed3a2f6db19f3d3b50441e2968b0552
henriquepablo <pablo269704@gmail.com>
2024-05-25 10:56:33 -0300

    feat: adding method that update tasks

--- src/main/java/com/lacerda/toDoList/controller/TasksController.java
+++ src/main/java/com/lacerda/toDoList/controller/TasksController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,9 +30,14 @@ public class TasksController {
 	}
 
 	@PostMapping
-	public Tasks crate(@RequestBody Tasks tasks) {
+	public Tasks create(@RequestBody Tasks tasks) {
 				
 		return taksService.create(tasks);
+	}
+	
+	@PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Tasks update(@PathVariable(value = "id") Long id, @RequestBody Tasks tasks) throws Exception {
+		return taksService.update(tasks, id);
 	}
 	
 	@DeleteMapping(value = "/{id}")
--- src/main/java/com/lacerda/toDoList/service/TasksService.java
+++ src/main/java/com/lacerda/toDoList/service/TasksService.java
@@ -26,6 +26,12 @@ public class TasksService {
 	}
 	
 	
+	public Tasks update(Tasks tasks, Long id) throws Exception {
+		Tasks entity = tasksRepository.findById(id).orElseThrow(() -> new Exception("Não encontrado"));
+		entity.setTaks(tasks.getTaks());
+		return tasksRepository.save(entity);		
+	}
+	
 	public void delete(Long id) throws Exception {
 		Tasks tasks = tasksRepository.findById(id).orElseThrow(() -> new Exception("Não encontrado"));
 		
