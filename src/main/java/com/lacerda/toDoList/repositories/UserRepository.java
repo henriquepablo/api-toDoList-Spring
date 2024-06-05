10900f8c63f0d2b4b8d24e7c96b527b9f689caf2
henriquepablo <pablo269704@gmail.com>
2024-06-05 17:01:28 -0300

    feat: method that return userByEmail

--- src/main/java/com/lacerda/toDoList/repositories/UserRepository.java
+++ src/main/java/com/lacerda/toDoList/repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package com.lacerda.toDoList.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lacerda.toDoList.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	
+	Optional<User> findByEmail(String email);
 }
