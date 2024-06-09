2287abfa5108ec2475eae217c0c4dbd4107dda91
henriquepablo <pablo269704@gmail.com>
2024-06-09 19:49:02 -0300

    refactor: adding atributes in the entity Tasks

--- src/main/java/com/lacerda/toDoList/model/Tasks.java
+++ src/main/java/com/lacerda/toDoList/model/Tasks.java
@@ -1,17 +1,37 @@
 package com.lacerda.toDoList.model;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
-@Entity(name = "tasks")
+@Entity
+@Table(name = "tasks")
 public class Tasks {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String taks;
+	
+	@Column(name = "task", length = 255)
+	private String tasks;
+	
+	@Column(name = "taksCreatedAt", length = 255)
+	private LocalDateTime taskCreatedAt;
+	
+	@Column(name = "data", length = 255)
+	private LocalDate data;
+	
+	@ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 	
 	public Tasks() {
 		
@@ -25,18 +45,42 @@ public class Tasks {
 		this.id = id;
 	}
 
-	public String getTaks() {
-		return taks;
+	public String getTasks() {
+		return tasks;
 	}
 
-	public void setTaks(String taks) {
-		this.taks = taks;
+	public void setTasks(String tasks) {
+		this.tasks = tasks;
+	}
+
+	public LocalDateTime getTaskCreatedAt() {
+		return taskCreatedAt;
+	}
+
+	public void setTaskCreatedAt(LocalDateTime taskCreatedAt) {
+		this.taskCreatedAt = taskCreatedAt;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
 	}
 
 	@Override
 	public String toString() {
-		return "Tasks [days=" + ", id=" + id + ", taks=" + taks + "]";
+		return "Tasks [id=" + id + ", tasks=" + tasks + ", taskCreatedAt=" + taskCreatedAt + ", data=" + data
+				+ ", user=" + user + "]";
 	}
-
-	
+		
 }
