ee81395f4ee7791bd2558638659aa582a5602805
henriquepablo <pablo269704@gmail.com>
2024-06-09 19:06:40 -0300

    refactor: changing the response of ResponseDTO

--- src/main/java/com/lacerda/toDoList/DTO/ResponseDTO.java
+++ src/main/java/com/lacerda/toDoList/DTO/ResponseDTO.java
@@ -1,5 +1,5 @@
 package com.lacerda.toDoList.DTO;
 
-public record ResponseDTO(String name, String token, String email) {
+public record ResponseDTO(Long id, String name, String token, String email) {
 
 }
