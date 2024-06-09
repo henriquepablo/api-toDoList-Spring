c85af8c4481d539e5a638d4ccbab0b9b33632f88
henriquepablo <pablo269704@gmail.com>
2024-06-09 20:01:59 -0300

    feat: function that return a task of the user

--- src/main/java/com/lacerda/toDoList/service/TasksService.java
+++ src/main/java/com/lacerda/toDoList/service/TasksService.java
@@ -8,27 +8,30 @@ import org.springframework.stereotype.Service;
 import com.lacerda.toDoList.model.Tasks;
 import com.lacerda.toDoList.repositories.TasksRepository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
 @Service
 public class TasksService {
 
 	@Autowired
 	TasksRepository tasksRepository;
 	
-	public List<Tasks> findAll() {
-
-		return tasksRepository.findAll();
+	public List<Tasks> findAll(Long id) {
+		
+		return tasksRepository.findTaskByUserId(id);
 
 	}
 	
 	public Tasks create(Tasks tasks) {
-
+		
 		return tasksRepository.save(tasks);
 	}
 	
 	
 	public Tasks update(Tasks tasks, Long id) throws Exception {
 		Tasks entity = tasksRepository.findById(id).orElseThrow(() -> new Exception("Não encontrado"));
-		entity.setTaks(tasks.getTaks());
+		entity.setTasks(tasks.getTasks());
 		return tasksRepository.save(entity);		
 	}
 	
