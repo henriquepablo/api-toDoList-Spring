80e710b523af460355d559aeb2ece3dfc6357220
henriquepablo <pablo269704@gmail.com>
2024-06-24 20:20:07 -0300

    feat: adding variables environment

--- src/main/java/com/lacerda/toDoList/repositories/TasksRepository.java
+++ src/main/java/com/lacerda/toDoList/repositories/TasksRepository.java
@@ -3,7 +3,6 @@ package com.lacerda.toDoList.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lacerda.toDoList.model.Tasks;
